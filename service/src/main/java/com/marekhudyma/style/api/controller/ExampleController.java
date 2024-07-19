09f1d038a6879c4e71310839148b30195cb0ac9b
Marek Hudyma <mhudyma@wayfair.com>
2024-07-19 22:33:57 +0200

    versions

--- service/src/main/java/com/marekhudyma/style/api/controller/ExampleController.java
+++ service/src/main/java/com/marekhudyma/style/api/controller/ExampleController.java
@@ -4,15 +4,11 @@ package com.marekhudyma.style.api.controller;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.time.Instant;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-@Log4j2
 public class ExampleController {
 
   @GetMapping(path = "/api/datetime", produces = APPLICATION_JSON_VALUE)
